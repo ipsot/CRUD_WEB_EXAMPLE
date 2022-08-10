6f39eeddb65c65c44da0d9ea41b5267c2e919bab
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 13:41:37 +0300

    Add files via upload

---
+++ Controllers/ClientBooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientWebApp2.Controllers
+{
+    public class ClientBooksController : Controller
+    {
+        // GET: ClientBooks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
